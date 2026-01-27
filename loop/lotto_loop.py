b265013fb9fc5becb416ab9fe1cd5f99c30352f7
tsaishengsheng-sketch <tsaishengsheng@gmail.com>
2026-01-27 22:19:53 +0800

    style: apply new gitignore to keep .py local only

--- loop/lotto_loop.py
+++
@@ -1,9 +0,0 @@
-"""
-檔名：lotto_loop.py
-功能：迴圈演算法練習
-學習重點：for/while 迭代、控制流程
-"""
-import random
-for i in range(1, 7):
-    lottoNum = random.randint(1, 49)
-    print(lottoNum, end = '')
--- loop/random_50_output.py
+++
@@ -1,9 +0,0 @@
-"""
-檔名：random_50_output.py
-功能：迴圈演算法練習
-學習重點：for/while 迭代、控制流程
-"""
-import random
-for i in range(1, 51):
-    randNum = random.randint(1, 49)
-    print(f"randNum:{randNum:4d}")
